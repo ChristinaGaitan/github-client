5798fdb13a3d62c7ce3798682ffddbf08656d8a2
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-23 17:15:14 -0600

    cache user info

--- src/auth/ClientProvider.tsx
+++ src/auth/ClientProvider.tsx
@@ -2,9 +2,17 @@ import React, { FC, PropsWithChildren } from "react";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { authFlowLink } from "./authFlowLink";
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    User: {
+      merge: true,
+    },
+  },
+});
+
 export const ClientProvider: FC<PropsWithChildren<{}>> = ({ children }) => {
   const client = new ApolloClient({
-    cache: new InMemoryCache(),
+    cache,
     link: authFlowLink,
   });
 
