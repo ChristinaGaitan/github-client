d03331120bcb92dd086badf5ad0806acf60e14cf
Christina Gaitan <christina.gaitan@gmail.com>
2025-01-24 09:42:15 -0600

    create getRepository query

---
+++ src/queries/types/getRepository.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: getRepository
+// ====================================================
+
+export interface getRepository_repository {
+  __typename: "Repository";
+  /**
+   * The Node ID of the Repository object
+   */
+  id: string;
+}
+
+export interface getRepository {
+  /**
+   * Lookup a given repository by the owner and repository name.
+   */
+  repository: getRepository_repository | null;
+}
+
+export interface getRepositoryVariables {
+  owner: string;
+  name: string;
+}
